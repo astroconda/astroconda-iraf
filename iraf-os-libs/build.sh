73f52caf1321ddc027dba624da5907dcb49ef615
James Turner <jturner@gemini.edu>
2016-10-21 10:48:03 -0300

    Oops, add missing ncurses lib (not just its copyright...).

--- iraf-os-libs/build.sh
+++ iraf-os-libs/build.sh
@@ -4,7 +4,7 @@
 # generalizing later, to pick up whatever major version is actually used
 # on any given build machine:
 libs="libXmu.so.6 libXt.so.6 libSM.so.6 libICE.so.6 libXext.so.6 libX11.so.6 \
-      libXau.so.6 libXdmcp.so.6 libXfixes.so.3 libXrender.so.1"
+      libXau.so.6 libXdmcp.so.6 libXfixes.so.3 libXrender.so.1 libncurses.so.5"
 
 oslib="/usr/lib"                # CentOS keeps 32-bit libs here
 osdoc="/usr/share/doc"          # CentOS keeps copyright notices etc. here
