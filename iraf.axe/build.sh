f13da7e7b7a29e29ed94ab3ca2869b592136e4e6
James Turner <jturner@gemini.edu>
2017-12-15 20:00:26 -0300

    Build old, working GSL 1.9 & link to it statically. Require new STSDAS 3.18 with updated aXe Python files & update pkg version to 2.6 to match.

--- iraf.axe/build.sh
+++ iraf.axe/build.sh
@@ -1,23 +1,42 @@
 # Install aXe binaries into STSDAS in the same way as the Ureka pkg-install.
-
-cd ccc
+# This must be built on CentOS >=6, for compatibility with the cfitsio build.
 
 # I'd have thought conda-build would take care of this but it appears not to:
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib"
 export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PREFIX/lib"
 
+set -e
+
+# Creating a dummy STSDAS directory as the install target avoids having IRAF
+# as a heavyweight build dependency when it's not needed (at the cost of this
+# script having to know the IRAF directory structure in the env, but that's
+# not likely to change):
+stdir=$PREFIX/iraf_extern/stsdas
+mkdir -p "$stdir/bin"
+
+echo "Build static GSL (old version needed by aXe)"
+
+cd gsl
+./configure --prefix="$PREFIX" --disable-shared --enable-static --build=x86
+make
+make install
+# here the GSL COPYRIGHT is covered by aXe's own GPL notice
+
+echo "Build aXe"
+
+cd ../aXe/ccc
 ./configure --prefix="$PREFIX" --with-cfitsio-prefix="$PREFIX" \
             --with-wcstools-prefix="$PREFIX/lib" --build=x86 \
-            --with-gsl-prefix="$PREFIX" || exit 1
+            --with-gsl-prefix="$PREFIX"
+make
 
-make || exit 1
-
-# Creating a dummy STSDAS directory avoids having IRAF as a heavyweight build
-# dependency when it's not needed (at the cost of this script having to know
-# the IRAF directory structure in the env, but that's not likely to change):
-stdir=$PREFIX/iraf_extern/stsdas
-mkdir -p "$stdir/bin" && cp -p bin/* "$stdir/bin/" || exit 1
+echo "Install aXe into STSDAS path"
+cp -p bin/* "$stdir/bin/"
 
 # Install obligatory licensing information (best kept in pkg with binaries):
-cp -p "$RECIPE_DIR/copyright.aXe" "$stdir/" || exit 1
+cp -p "$RECIPE_DIR/copyright.aXe" "$stdir/"
+
+# Newer versions of the Python files in iraf/acesrc are also required, as of
+# 2017, but those have been updated in the STSDAS 3.18 source rather than here,
+# as they live in STSDAS from the outset.
 
